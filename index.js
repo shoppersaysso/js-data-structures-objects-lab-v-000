396464612349d348f872760bcc796620a1e23bb2
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 03:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 // this function should work the same as updateDriverWithKeyAndValue()
 // but it should mutate the driver parameter passed in.
 //
-// deleteFromDriverByKey() - this function should take in a driver object and a key.
+function deleteFromDriverByKey(){
+
+};
+//this function should take in a driver object and a key.
 // It should delete the key/value pair for the key that was passed in from the driver object.
 // This should all not actually mutate the driver passed in.
 //
