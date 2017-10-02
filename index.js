012a2b40ee51053b418dce34aadc319dba26eac6
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 04:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromDriverByKey(driver, key){
 };
 
 // function destructivelyDeleteFromDriverByKey(driver, key){
-//   delete driver[key] = value;
+//   delete driver[key];
 //
 //   return driver;
 // };
