78810dfd9e50be2af475658a6f76cc3b035ee35a
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 03:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// // Write your solution in this file!
+const driver = {
+  name: 'Sam',
+  address: '11 Broadway',
+};
 // updateDriverWithKeyAndValue()- this function should take in a driver object, a key and a value.
 // The function should not mutate the driver parameter and return a new driver that has an updated value for the key passed in.
 //
